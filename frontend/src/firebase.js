a9c7b9cf30fef24c91ff3a15633521daad7e8d79
gusqkr <hyeon05@ewha.ac.kr>
2026-02-08 16:39:20 +0900

    setting: 프로젝트 초기 뼈대 생성 및 파이어베이스 연동

---
+++ frontend/src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEkF0N2V0ddUYUscaAPKVIxIqNeMchwV4",
+  authDomain: "putfruitsonmycake.firebaseapp.com",
+  projectId: "putfruitsonmycake",
+  storageBucket: "putfruitsonmycake.firebasestorage.app",
+  messagingSenderId: "451859039394",
+  appId: "1:451859039394:web:c976ce86005ef3d0dcc3fb",
+  measurementId: "G-ES19PN0S2K"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export const googleProvider = new GoogleAuthProvider();
+
+export default app;
